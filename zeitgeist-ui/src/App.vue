c8b8fe6d56f2131617dc13e3f81e862f3e564961
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2020-10-02 17:07:15 +0200

    Navbar, website background, logo

--- zeitgeist-ui/src/App.vue
+++ zeitgeist-ui/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Zeitgeist Logo" src="./assets/zeitgeist_logo.png" style="margin-top: 100px">
+    <Header></Header>
+    <div class="left-container">
+      <div class="row">
+        <div class="col-lg-2 col-lg-10 ">
+          <img class="img-fluid" style="width: 50%; height: 50%" src="./assets/logo.svg">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+  import Header from "./components/Header";
+
   export default {
     name: 'App',
-    components: {},
-  };
+    components: {Header},
+    data() {
+      return {
+      }
+    }
+  }
 </script>
 
 <style>
+  body{
+    background-image: url("./assets/backgournd3.jpg");
+    background-position: left;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+  }
 </style>
